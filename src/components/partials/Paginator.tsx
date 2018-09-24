867e516ab80865c6074ae8e7b64e0f1fac2604dc
ThomasBird <thomasbird@ThomasBirds-MacBook-Pro.local>
2018-09-23 21:53:47 -0400

    Tweaking

--- src/components/partials/Paginator.tsx
+++ src/components/partials/Paginator.tsx
@@ -31,7 +31,6 @@ export class Paginator extends React.Component<PaginatorProps, State> {
     this.setState({
       totalPages: Math.ceil(this.props.paginationData.total / this.props.paginationData.per_page)
     });
-    console.log("this.state.totalPages", this.state.totalPages);
   }
 
   public render(): JSX.Element {
