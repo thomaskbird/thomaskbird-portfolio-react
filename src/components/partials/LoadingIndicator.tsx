27734ee380dd594b63c3fb5f92579b00ed263be9
Thomas Bird <tbird@control-tec.com>
2019-01-07 12:10:48 -0500

    Fixing loading indicator

--- src/components/partials/LoadingIndicator.tsx
+++ src/components/partials/LoadingIndicator.tsx
@@ -34,7 +34,7 @@ export class LoadingIndicator extends React.Component<LoadingIndicatorProps, Sta
             );
         } else {
             return (
-                <div />
+                <></>
             );
         }
     }
