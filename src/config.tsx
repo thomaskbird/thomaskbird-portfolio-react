121340d798c1179d316c9448b0dc1940840ce51f
Tom Bird <thomas.bird1984@gmail.com>
2019-05-16 03:08:48 -0400

    Adding config.tsx to git

---
+++ src/config.tsx
@@ -0,0 +1,7 @@
+export interface Configuration {
+  apiUrl: string;
+}
+
+export const config: Configuration = {
+  apiUrl: "http://api.thomaskbird.com/api"
+};
--- src/services/Api.tsx
+++ src/services/Api.tsx
@@ -1,5 +1,5 @@
 import { config } from "../config";
-import {ApiConfig} from "../interfaces";
+import { ApiConfig } from "../interfaces";
 
 export class Api {
     private config: any;
