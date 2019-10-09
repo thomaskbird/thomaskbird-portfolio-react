d285cc883f139f5b08db2eb52831860c7465bf75
Tom Bird <thomas.bird1984@gmail.com>
2019-10-09 16:00:55 -0400

    Disabled link in print view

--- src/components/pages/ResumePrintView.tsx
+++ src/components/pages/ResumePrintView.tsx
@@ -64,7 +64,7 @@ export class ResumePrintView extends React.Component<ResumePrintViewProps, State
                 <div className={"container-outer ResumePrintView"}>
                     <div className={"BrandContactInfo"}>
                         <div className={"BrandContactInfo-left"}>
-                            <Brand/>
+                            <Brand linked={false} />
                             <ContactInfo/>
                         </div>
                         <div className={"BrandContactInfo-right"}>
--- src/components/partials/Brand.tsx
+++ src/components/partials/Brand.tsx
@@ -2,12 +2,18 @@ import * as React from "react";
 import { Link } from "react-router-dom";
 import "./Brand.scss";
 
-interface BrandProps {}
+interface BrandProps {
+  linked?: boolean;
+}
 
 interface State {}
 
 export class Brand extends React.Component<BrandProps, State> {
   public static readonly displayName = "App component";
+
+  private static defaultProps = {
+    linked: true,
+  };
 
   constructor(
     props: BrandProps,
@@ -19,13 +25,18 @@ export class Brand extends React.Component<BrandProps, State> {
   }
 
   public render(): JSX.Element {
-    return (
+    return this.props.linked ? (
       <Link to="/">
         <span className={"brand"}>
           <span className={"brand-name"}>ThomasBird</span>
           <span className={"brand-title"}>FullStackDeveloper</span>
         </span>
       </Link>
+    ) : (
+      <span className={"brand"}>
+        <span className={"brand-name"}>ThomasBird</span>
+        <span className={"brand-title"}>FullStackDeveloper</span>
+      </span>
     );
   }
 }
